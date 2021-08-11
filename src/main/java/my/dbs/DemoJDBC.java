d2e9ded40d211cef19351872623ebb5e94003f65
Квасов Сергей Сергеевич <sskvasov@dev.vtb.ru>
2021-08-11 14:28:22 +0300

    first

--- src/main/java/my/dbs/DemoJDBC.java
+++ src/main/java/my/dbs/DemoJDBC.java
@@ -4,17 +4,16 @@ import java.sql.*;
 
 public class DemoJDBC {
     public static void main(String[] args) throws SQLException {
-
-        Connection connection = DriverManager.getConnection("jdbc:sqlserver://yand.dyndns.org;databaseName=AdventureWorks",
-                "northwind", "northwind");
-        String sql = "SELECT ProductID, Name, ProductNumber, ListPrice FROM Production.Product ";
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(sql);
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("Name") + " " + resultSet.getDouble("ListPrice"));
+        try (
+                Connection connection = DriverManager.getConnection("jdbc:sqlserver://yand.dyndns.org;databaseName=AdventureWorks",
+                        "northwind", "northwind");) {
+            String sql = "SELECT ProductID, Name, ProductNumber, ListPrice FROM Production.Product ";
+            Statement statement = connection.createStatement();
+            try (ResultSet resultSet = statement.executeQuery(sql);) {
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("Name") + " " + resultSet.getDouble("ListPrice"));
+                }
+            }
         }
-        resultSet.close();
-        statement.close();
-        connection.close();
     }
 }
