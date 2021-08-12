b6c6562e02fdb2ebcf80e845cb66b48dbe635086
Квасов Сергей Сергеевич <sskvasov@dev.vtb.ru>
2021-08-12 14:01:11 +0300

    first

---
+++ src/main/java/my/dbs/DemoJPA.java
@@ -0,0 +1,26 @@
+package my.dbs;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+
+// Файл persistence.xml должен находиться на classpath'е
+// в папке META-INF. Т.е. в мавеновском проекте он
+// должен быть в папке src/main/Resources/META-INF.
+
+public class DemoJPA {
+
+    public static void main(String[] args) {
+        EntityManagerFactory factory
+                = Persistence.createEntityManagerFactory("my_jpa");
+
+        EntityManager em = factory.createEntityManager();
+        List<Product> products
+                = em.createQuery("SELECT p FROM Product p").getResultList();
+        products.forEach( p -> {
+            System.out.println(p.getName() + " " + p.getPrice());
+        });
+    }
+}
